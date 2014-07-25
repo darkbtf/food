5652250061d0aca06e0d94f87b1079784b4984b8
Po-Cheng Chu <darkbtf@gmail.com>
2014-07-25 14:57:16 +0800

    response is normal now

--- api/controllers/RobotController.js
+++ api/controllers/RobotController.js
@@ -5,7 +5,18 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var request = require('request');
+
 module.exports = {
-	
+  index: function(req, res) {
+    console.log(req.body.text);
+    var obj = {};
+    console.log(req.body);
+    if (req.body.user_name != 'slackbot') {
+      obj.text = '你好~ 今天想吃什麼？'
+    }
+    
+    return res.send(obj); 
+  }
 };
 
