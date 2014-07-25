e8c4ea9681bd54f039b84f7fbfcc588ce879a2ce
Po-Cheng Chu <darkbtf@gmail.com>
2014-07-25 14:15:39 +0800

    robot added

---
+++ api/controllers/RobotController.js
@@ -0,0 +1,11 @@
+/**
+ * RobotController
+ *
+ * @description :: Server-side logic for managing robots
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
